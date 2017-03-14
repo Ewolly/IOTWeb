d334936c27888d5b4843946a356ad5b1a1e579ae
Ewolly <ewollypop@gmail.com>
2017-03-14 18:02:41 +1300

    Oauth
    stuff

---
+++ Oauth.py
@@ -0,0 +1,24 @@
+from oauth2client.client import OAuth2WebServerFlow
+import webbrowser
+from oauth2client.file import Storage
+import httplib2
+
+flow = OAuth2WebServerFlow(
+    redirect_uri='http://iot.duality.co.nz/oauth2callback',
+    scope='email',
+    user_agent='my-sample/1.0')
+
+
+authorize_url = flow.step1_get_authorize_url()
+webbrowser.open(authorize_url, new=2)
+
+credentials = flow.step2_exchange(raw_input("code: "))
+storage = Storage('credentials')
+storage.put(credentials)
+
+credentials = storage.get()
+
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+print http
