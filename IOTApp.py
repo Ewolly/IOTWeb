1cd31c5a62197f2858607b7ee892bc30a108d80f
Jamie Harvey <jharvey@duality.co.nz>
2017-03-23 22:27:28 +1300

    Include API blueprint in main app.

--- IOTApp.py
+++ IOTApp.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 from authentication import auth
 from werkzeug.contrib.fixers import ProxyFix
 from iot_db import init_app
+from iot_api import iot_api
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 app.register_blueprint(auth)
+app.register_blueprint(iot_api, url_prefix='/api/1')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 init_app(app)
