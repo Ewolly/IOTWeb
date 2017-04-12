6720c2bed0f758d7dbd79112ff04f8227519a4f0
Ewolly <ewollypop@gmail.com>
2017-04-12 15:31:06 +1200

    added treading

--- iot_sockets.py
+++ iot_sockets.py
@@ -1,4 +1,6 @@
+import socket
 import SocketServer
+import threading
 import json
 import iot_db
 from datetime import datetime
@@ -61,7 +63,33 @@ class DeviceTCPHandler(SocketServer.StreamRequestHandler):
             self.wfile.write(self.data)
 
 
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024)
+        cur_thread = threading.current_thread()
+        response = "{}: {}".format(cur_thread.name, data)
+        self.request.sendall(response)
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+def client(ip, port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    try:
+        sock.sendall(message)
+        response = sock.recv(1024)
+        print "Received: {}".format(response)
+    finally:
+        sock.close()
+
+
 def start_device_server(port):
-    SocketServer.TCPServer.allow_reuse_address = True
-    server = SocketServer.TCPServer(('localhost', port), DeviceTCPHandler)
-    server.serve_forever()
+    HOST, PORT = "localhost", 0
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+
