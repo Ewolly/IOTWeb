0b50cfe3b34e12388359604b04cd239c82f076f1
Jamie Harvey <jharvey@duality.co.nz>
2017-04-18 21:12:43 +1200

    update error messages, add flask app_context

--- iot_sockets.py
+++ iot_sockets.py
@@ -4,79 +4,82 @@ import threading
 import json
 import iot_db
 from datetime import datetime
+from flask import current_app
 
 class DeviceTCPHandler(SocketServer.StreamRequestHandler):
     def handle(self):
-        self.request.settimeout(30)
-        try:
-            self.data = self.rfile.readline().strip()
-        except:
-            print "No initial connection."
-            return
-        if self.data == '':
-            return
-        
-        self.message = None
-        try:
-            self.message = json.loads(self.data)
-        except:
-            self.wfile.write(json.dumps({'error': 'problem parsing JSON'}))
-            return
-
-        self.wfile.write(json.dumps(
-                {'error': '1'}))
-
-        device_id = self.message.get('id', None)
-        device_token = self.message.get('token', None)
-        if device_id is None or device_token is None:
-            self.wfile.write(json.dumps(
-                {'error': 'request must have id and token'}))
-            return
-
-        self.wfile.write(json.dumps(
-                {'error': '2'}))
-
-        device = iot_db.Devices.query.get(device_id)
-        
-        if device is None:
-            self.wfile.write(json.dumps(
-                {'error': 'request id does not exist in the database'}))
-            return
-
-        self.wfile.write(json.dumps(
-            {'error': '3'}))
-
-        if device_token != device.token:
-            self.wfile.write(json.dumps(
-                {'error': 'request token and actual token do not match'}))
-            return
-
-        self.wfile.write(json.dumps(
-                {'error': '4'}))
-
-        device.last_checked = datetime.utcnow()
-        device.ip_address = self.client_address[0]
-        port = self.client_address[1]
-        iot_db.update_db()
-
-        # print '{}\'s device "{}" connected from {}.'.format(
-        #     device.user.email
-        #     device.friendly_name,
-        #     self.client_address[0])
-
-        print '{}: {} ({})'.format(device_id, device_token, 
-            self.client_address[0])
-        
-        while True:
+        with current_app.app_context():    
+            self.request.settimeout(30)
             try:
-                self.data = self.rfile.readline()
+                self.data = self.rfile.readline().strip()
             except:
-                print "Connection timed out."
+                print "No initial connection."
                 return
-
             if self.data == '':
                 return
-            self.wfile.write(self.data)
+            
+            self.message = None
+            try:
+                self.message = json.loads(self.data)
+            except:
+                self.wfile.write(json.dumps({'error': 'problem parsing JSON'}))
+                return
+
+            self.wfile.write(json.dumps({'info': 1}))
+
+            device_id = self.message.get('id', None)
+            device_token = self.message.get('token', None)
+            if device_id is None or device_token is None:
+                self.wfile.write(json.dumps(
+                    {'error': 'request must have id and token'}))
+                return
+
+            self.wfile.write(json.dumps({'info': 2}))
+
+            try:
+                device = iot_db.Devices.query.get(device_id)
+            except Exception as e:
+                self.wfile.write(json.dumps({'error': repr(e)}))
+                return
+
+
+            if device is None:
+                self.wfile.write(json.dumps(
+                    {'error': 'request id does not exist in the database'}))
+                return
+
+            self.wfile.write(json.dumps({'info': 3}))
+
+            if device_token != device.token:
+                self.wfile.write(json.dumps(
+                    {'error': 'request token and actual token do not match'}))
+                return
+
+            self.wfile.write(json.dumps({'info': 4}))
+
+            device.last_checked = datetime.utcnow()
+            device.ip_address = self.client_address[0]
+            port = self.client_address[1]
+            iot_db.update_db()
+
+            # print '{}\'s device "{}" connected from {}.'.format(
+            #     device.user.email
+            #     device.friendly_name,
+            #     self.client_address[0])
+
+            print '{}: {} ({})'.format(device_id, device_token, 
+                self.client_address[0])
+            
+            while True:
+                try:
+                    self.data = self.rfile.readline()
+                except:
+                    print "Connection timed out."
+                    return
+
+                if self.data == '':
+                    return
+                self.wfile.write(self.data)
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
