ba5c2119fd83b7d6d11b2305b970f8726eb35750
Ewolly <ewollypop@gmail.com>
2017-04-18 19:19:30 +1200

    fraser made changes

--- iot_sockets.py
+++ iot_sockets.py
@@ -80,9 +80,13 @@ class DeviceTCPHandler(SocketServer.StreamRequestHandler):
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
+    daemon_threads = True
+    allow_reuse_address = True
 
 def start_device_server(port):
     HOST, PORT = "0.0.0.0", 8091
     server = ThreadedTCPServer((HOST, PORT), DeviceTCPHandler)
-    server.serve_forever()
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    
