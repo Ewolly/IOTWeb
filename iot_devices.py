761cd5cc0c4921dd3aba52c1d8361738e9515a06
Jamie Harvey <jharvey@duality.co.nz>
2017-04-27 20:12:05 +1200

    fix to previous commit

--- iot_devices.py
+++ iot_devices.py
@@ -2,18 +2,18 @@ from flask import Blueprint
 from flask import request, url_for, render_template, session, flash
 import iot_db
 from datetime import datetime
-import devices
+from devices import *
 
 iot_devices = Blueprint('iot_devices', __name__)
-modules = [
-    devices.unknown,
-    devices.smartplug,
-    devices.bluetooth,
-    devices.usb,
-    devices.infrared,
-    devices.industrial,
-    devices.multiplug,
-    devices.audio,
+module_names = [
+    devices.unknown.name,
+    devices.smartplug.name,
+    devices.bluetooth.name,
+    devices.usb.name,
+    devices.infrared.name,
+    devices.industrial.name,
+    devices.multiplug.name,
+    devices.audio.name,
 ]
 
 @iot_devices.route('/devices')
@@ -27,4 +27,4 @@ def list_devices():
         flash('User does not exist.', 'error')
         return redirect(url_for('auth.login_request'), 303)
     return render_template('devices.html', 
-        devices=user.devices, module_names=(x.name for x in modules))
+        devices=user.devices, module_names=module_names)
