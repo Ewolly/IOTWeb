7139a4e08ce58f8daef33151b1d6f177e566342b
Jamie Harvey <jharvey@duality.co.nz>
2017-04-27 22:05:03 +1200

    attempt 7

--- devices/__init__.py
+++ devices/__init__.py
@@ -1,10 +1,19 @@
-__all__ = [
-    'unknown',
-    'smartplug',
-    'bluetooth',
-    'usb',
-    'infrared',
-    'industrial',
-    'multiplug',
-    'audio',
+import unknown
+import smartplug
+import bluetooth
+import usb
+import infrared
+import industrial
+import multiplug
+import audio
+
+device_modules = [
+    unknown.Unknown,
+    smartplug.SmartPlug,
+    bluetooth.Bluetooth,
+    usb.USB,
+    infrared.Infrared,
+    industrial.Industrial,
+    multiplug.MultiPlug,
+    audio.Audio,
 ]
--- iot_devices.py
+++ iot_devices.py
@@ -2,19 +2,9 @@ from flask import Blueprint
 from flask import request, url_for, render_template, session, flash
 import iot_db
 from datetime import datetime
-from devices import *
+from devices import device_modules
 
 iot_devices = Blueprint('iot_devices', __name__)
-modules = [
-    unknown.Unknown,
-    smartplug.SmartPlug,
-    bluetooth.Bluetooth,
-    usb.USB,
-    infrared.Infrared,
-    industrial.Industrial,
-    multiplug.MultiPlug,
-    audio.Audio,
-]
 
 @iot_devices.route('/devices')
 def list_devices():
@@ -27,4 +17,5 @@ def list_devices():
         flash('User does not exist.', 'error')
         return redirect(url_for('auth.login_request'), 303)
     return render_template('devices.html', 
-        devices=user.devices, module_names=[x.name for x in modules])
+        devices=user.devices, 
+        module_names=[x.name for x in device_modules])
