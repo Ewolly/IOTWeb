75f33f51a74ad350928c14b34e2ea152f38b3a75
Jamie Harvey <jharvey@duality.co.nz>
2017-04-27 22:01:27 +1200

    make instance methods static

--- devices/smartplug.py
+++ devices/smartplug.py
@@ -3,8 +3,10 @@ import iot_db
 class SmartPlug(object):
     name = 'smartplug'
 
+    @staticmethod
     def device_details(device):
         return None
 
+    @staticmethod
     def set_plug(device, status):
         pass
--- devices/unknown.py
+++ devices/unknown.py
@@ -3,5 +3,6 @@ import iot_db
 class Unknown(object):
     name = 'unknown'
 
+    @staticmethod
     def device_details(device):
         return None
