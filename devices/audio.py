fef9608df974935500b9b050fc56e900c1dee9d7
Jamie Harvey <jharvey@duality.co.nz>
2017-05-16 22:35:58 +1200

    update calls

--- devices/audio.py
+++ devices/audio.py
@@ -1,3 +1,21 @@
 from smartplug import *
 
 name = 'audio'
+
+def device_details(device):
+    return_dict = {
+        'connected': device.client_id is not None,
+        'speaker_status': True,
+        'mic_status': False,
+        'speaker_VU': 20,
+        'mic_VU': 80
+    }
+    if (device.client_id is not None):
+        return_dict.append({
+            'client_id': device.client_id,
+            'client_name': device.client.friendly_name,
+            'ip_address': device.ip_address,
+            'port': device.port
+        })
+
+    return return_dict
--- devices/smartplug.py
+++ devices/smartplug.py
@@ -1,10 +1,11 @@
 name = 'smartplug'
 
 def device_details(device):
-    return {
-        'plug_status': device.plug_status,
-        'current_consumption': device.current_consumption
-    }
+    return_dict = {'plug_status': device.plug_status}
+    if (device.current_consumption is not None):
+        return_dict.append({'current_consumption': device.current_consumption})
+    
+    return return_dict
 
 def set_plug(device, status):
     pass
