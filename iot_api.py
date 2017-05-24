6f77200f89ace0310a49612518d939d9656ce9e0
Jamie Harvey <jharvey@duality.co.nz>
2017-05-24 18:27:06 +1200

    connect code

--- iot_api.py
+++ iot_api.py
@@ -178,7 +178,14 @@ def deregister_device(device_id):
 # ---------------------------
 @iot_api.route('/device/<int:device_id>/connect', methods=['POST'])
 def connect_device(device_id):
-    pass
+    user, device, err_msg = check_device(
+        request.headers.get('email'), 
+        request.headers.get('password'),
+        device_id)
+    if err_msg is not None:
+        return make_response(jsonify({'error': err_msg}), 400)
+
+    return make_response(jsonify({'status': 'success'}), 200)
 
 @iot_api.route('/device/<int:device_id>/disconnect', methods=['DELETE'])
 def disconnect_device(device_id):
