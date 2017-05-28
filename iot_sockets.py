b647c112722c9c95a4a59478959967d4177d7984
Ewolly <ewollypop@gmail.com>
2017-05-28 17:40:32 +1200

    jamies worse changes

--- iot_sockets.py
+++ iot_sockets.py
@@ -213,6 +213,7 @@ class DeviceHandler(LineReceiver, TimeoutMixin):
         self.state = 'MSG'
 
     def handle_MESSAGE(self, line):
+        
         from IOTApp import app
         with app.app_context():
             device = iot_db.Devices.query.get(self.device_id)
